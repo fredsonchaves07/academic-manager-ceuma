35b962aeee4f5c4ecf9848f4f3ef03893d918aa9
Fredson Chaves <fredsonchaves07@gmail.com>
2020-06-14 19:48:49 -0300

    feat: database connection pool configuration

--- app/controllers/courses.js
+++ app/controllers/courses.js
@@ -8,6 +8,13 @@ module.exports = {
     },
 
     post(req, res){
-        return 
+        const keys = Object.keys(req.body)
+
+        for (key of keys) {
+            if (req.body[key] == "") {
+                return res.send('Please, fill all fieds')
+            }
+        }
+
     }
 }
