c52a394afca4ce24f86ce1e65d490b202381a231
Fredson Chaves <fredsonchaves07@gmail.com>
2020-06-13 21:49:34 -0300

    fix: initial form configuration

--- app/router.js
+++ app/router.js
@@ -1,10 +1,14 @@
 const express = require('express')
+const students = require('./controllers/students')
 
 const routes = express.Router()
 
 
-routes.get('/', function(req, res){
+routes.get('/', (req, res) =>{
     return res.render('index')
 })
+
+routes.get('/students', students.index)
+routes.get('/students/create', students.create)
 
 module.exports = routes
